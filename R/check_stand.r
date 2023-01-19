850873ed3e3408cc9528fa38d0fbd3bd7e19fcbe
Roberto Molowny-Horas <roberto@creaf.uab.es>
2023-01-19 10:55:20 +0100

    Work on check_stand

--- R/check_stand.r
+++ R/check_stand.r
@@ -21,13 +21,6 @@ check_stand <- function(a) {
   b <- a %>% pull(saplings)
   saplingsp <- unique(unlist(sapply(b, function(x) unique(x$species))))
 
-
-
-
-
-
-
-
-
-
+  return(list(species.adults = adults,
+              species.saplingsp = saplingsp))
 }
