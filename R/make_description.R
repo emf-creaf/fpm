a2cb6df4f9b87fa9473eaa3ccc8c3b0ee9a9feeb
Roberto Molowny-Horas <roberto@creaf.uab.es>
2024-06-28 11:18:54 +0200

    Nothing.

---
+++ R/make_description.R
@@ -0,0 +1,20 @@
+usethis::use_description(
+  check_name = T,
+  fields = list(
+    Package = "fpm",
+    Version = "0.1.0",
+    Title = "Forest population modelling",
+    Description = "Modelling of population dynamics of forest data from national inventories",
+    `Authors@R` = person(given = "Roberto",
+                         family = "Molowny-Horas",
+                         email = "roberto@creaf.uab.es",
+                         role = c("aut", "cre"),
+                         comment = "https://orcid.org/0000-0003-2626-6379",
+
+
+    ),
+    Language = "en",
+    License = "GPL v3",
+    Lazydata = TRUE
+  )
+)
