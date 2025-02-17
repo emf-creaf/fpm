3e584ffc493cf3349278110a5e5d9a030b9009c1
Roberto Molowny-Horas <roberto@creaf.uab.es>
2025-02-17 13:22:44 +0100

    Added calculation of R3 (a kind of proxy for tree trunk volume) to calc_descriptive.R

--- R/calc_descriptive.R
+++ R/calc_descriptive.R
@@ -1,7 +1,8 @@
-#' Calculates basal area per plot and species.
+#' Calculates structural parameters per plot and species.
 #'
 #' @description
-#' It calculates the basal area and number of trees, total and per species, for a plot.
+#' It calculates the basal area, number of trees and sum of cube of tree radius,
+#' total and per species, for a plot.
 #'
 #' @param a a \code{sf} object containing a single row.
 #' @param param param a named \code{list} of parameters (see \code{Details} below).
@@ -9,9 +10,8 @@
 #'
 #' @return
 #' The input \code{sf} object with new fields added, namely \code{ba},
-#' \code{ntrees}, \code{ba_species} and \code{ntrees_species}, corresponding
-#' to the total basal area (m2) and total number of trees, plus basal area and
-#' number of trees per species, respectively.
+#' \code{ntrees} and \code{R3} (total values), and \code{ba_species}, \code{ntrees_species}
+#' and \code{R3_species} (per species).
 #'
 #' @details
 #' This function is used by \code{update_stands}. Inputs are not checked for correctness.
@@ -26,7 +26,7 @@ calc_descriptive <- function(a, param = list()) {
 
 
   # Other parameters.
-  ba <- ntrees <- data.frame()
+  ba <- ntrees <- R3 <- data.frame()
   stand_type <- a$stand_type
   cnst <- pi/40000
 
@@ -36,24 +36,41 @@ calc_descriptive <- function(a, param = list()) {
   if (country == "spain") {
     if (length(b) > 0) {
       if (stand_type == "individual") {
-        b$factor_diam <- factor_diam_IFN(b$dbh)
 
-        y <- b |> dplyr::group_by(species) |> dplyr::summarise(ba = sum(factor_diam * dbh^2) * cnst)
+        b <- b |> dplyr::mutate(factor_diam = factor_diam_IFN(b$dbh)) |> dplyr::group_by(species)
+
+        # Basal area.
+        y <- b |> dplyr::summarise(ba = sum(factor_diam * dbh^2) * cnst)
         ba <- split(y$ba, y$species)
 
-        y <- b |> dplyr::group_by(species) |> dplyr::summarise(ntrees = sum(factor_diam))
+        # Number of trees.
+        y <- b |> dplyr::summarise(ntrees = sum(factor_diam))
         ntrees <- split(y$ntrees, y$species)
 
+        # Cube radius, as a proxy for trunk volume.
+        y <- b |> dplyr::summarise(R3 = sum(factor_diam * (dbh/200)^3))
+        R3 <- split(y$R3, y$species)
+
+
       } else if (stand_type == "ipm") {
+
+        # Basal area.
         ba <- sapply(names(b), function(j) quadrature(b[[j]] * x[[j]]^2, h[[j]]) * cnst, simplify = F)
+
+        # Number of trees.
         ntrees <- sapply(names(b), function(j) quadrature(b[[j]], h[[j]]), simplify = F)
-if (sum(is.na(unlist(ntrees)))>0) browser()
+
+        # Cube radius, as a proxy for trunk volume.
+        R3 <- sapply(names(b), function(j) quadrature(b[[j]] * (x[[j]]/200)^3, h[[j]]), simplify = F)
+
       }
 
       a$ba_species[[1]] <- ba
       a$ba <- sum(unlist(ba))
       a$ntrees_species[[1]] <- ntrees
       a$ntrees <- sum(unlist(ntrees))
+      a$R3_species[[1]] <- R3
+      a$R3 <- sum(unlist(R3))
 
     }
   } else if (country == "usa") {
